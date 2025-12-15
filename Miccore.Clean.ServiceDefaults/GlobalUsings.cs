0c8f1b0b17d3ca54b4b12a5de4e4f484d7e593a4
Manuel Mbakop <mbakopmanuel@hotmail.com>
2025-12-14 23:15:48 -0500

    [feat]: Ajout du serviceDefault package dans la gateway

--- Miccore.Clean.ServiceDefaults/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-// Global using directives for Miccore.Clean.ServiceDefaults
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
